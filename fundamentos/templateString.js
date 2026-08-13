3ad07adb4af5a3a3589bb0a4ce209b735d3ec8fd
Johnny <johnny.datascientist@gmail.com>
2026-08-13 16:45:00 -0300

    added templateString.js

---
+++ fundamentos/templateString.js
@@ -0,0 +1,8 @@
+const nome = "Johnny";
+const sobrenome = "Matos";
+
+console.log(`Olá ${nome} ${sobrenome}!`);
+
+const n1 = 2;
+const n2 = 3;
+console.log(`${n1} + ${n2} = ${n1 + n2}`); // faz operações também. Funciona igual f-string do python
