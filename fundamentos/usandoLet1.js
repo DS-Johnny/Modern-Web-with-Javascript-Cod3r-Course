db16272da20d542000a22c7e921e36419e564ffe
Johnny <johnny.datascientist@gmail.com>
2026-08-26 17:01:30 -0300

    Added let and var in loop

--- fundamentos/usandoLet1.js
+++ fundamentos/usandoLet1.js
@@ -1,6 +1,6 @@
 var numero = 1;
 {
   let numero = 2;
-  console.log("dentro=", numero);
+  console.log("dentro=", numero); // apenas escopo de dentro da função
 }
-console.log("fora =", numero);
+console.log("fora =", numero); // apenas escopo de fora da função
---
+++ fundamentos/usandoLetEmLoop1.js
@@ -0,0 +1,7 @@
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+console.log(i); // i só está visível dentro do escopo do for
+
+// var não tem escopo de bloco, let tem
